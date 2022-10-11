fb7c9d0a32111052d0c48bdad9886cf9175a7a86
eshitakar <eshitakar3@gmail.com>
2022-10-11 09:34:50 -0400

    Reformatting IO

--- uncountable-front-end/components/IO.js
+++ uncountable-front-end/components/IO.js
@@ -7,6 +7,7 @@ import React from 'react';
 
 /*Basic Material UI imports*/
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
 /*Accordion imports*/
@@ -35,88 +36,92 @@ export default function IO(props) {
     // stylistically, I chose to use accordions to hide input/output to prevent the user from being overwhelmed
     // the choice to use tables for rendering data came from tables being an easy-on-the-eyes way to view data
     return(
-        <Box sx={{width: '100%' }}>
+        < Grid container spacing={2}>
             {/*creating accordion view for inputs*/}
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Typography sx={{color: 'secondary.dark'}}>Inputs</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    {/*iterate through inputs to render a table per input category*/}
-                    {parsed_inputs.map(input => {
-                        var elems = input[1]
-                        //only render category input rows if they were added to experiment
-                        if (elems.length > 0) {
-                            return(
-                                <TableContainer component={Paper}>
-                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                    <TableHead>
-                                    <TableRow sx={{bgcolor: "secondary.light"}}>
-                                        <TableCell sx={{fontWeight: 'bold'}}>{input[0]}</TableCell>
-                                        <TableCell sx={{fontWeight: 'bold'}} align="right">Amount</TableCell>
+            <Grid item xs={6}>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography sx={{color: 'secondary.dark'}}>Inputs</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        {/*iterate through inputs to render a table per input category*/}
+                        {parsed_inputs.map(input => {
+                            var elems = input[1]
+                            //only render category input rows if they were added to experiment
+                            if (elems.length > 0) {
+                                return(
+                                    <TableContainer sx={{ width: "100%" }} component={Paper}>
+                                    <Table aria-label="simple table">
+                                        <TableHead>
+                                        <TableRow sx={{bgcolor: "secondary.light"}}>
+                                            <TableCell sx={{fontWeight: 'bold'}}>{input[0]}</TableCell>
+                                            <TableCell sx={{fontWeight: 'bold'}} align="right">Amount</TableCell>
+                                        </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                        {/* for each specific input, render a row with amount data*/}
+                                        {elems.map((row) => {
+                                            return(
+                                                <TableRow
+                                                    key={row[0]}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell component="th" scope="row">{row[0]}</TableCell>
+                                                    <TableCell align="right">{row[1]}</TableCell>
+                                                </TableRow>
+                                            )
+                                        })}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                                );
+                            }
+                        })}
+                    </AccordionDetails>
+                </Accordion>
+            </Grid>
+            {/*creating accordion view for outputs*/}
+            <Grid item xs={6}>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography sx={{color: 'secondary.dark'}}>Outputs</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <TableContainer sx={{ width: "100%" }} component={Paper}>
+                            <Table aria-label="simple table">
+                                <TableHead>
+                                    <TableRow sx={{bgcolor: 'secondary.light'}}>
+                                        <TableCell sx={{fontWeight: "bold"}}>Measurements</TableCell>
+                                        <TableCell sx={{fontWeight: 'bold'}} align="right"></TableCell>
                                     </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                    {/* for each specific input, render a row with amount data*/}
-                                    {elems.map((row) => {
+                                </TableHead>
+                                <TableBody> 
+                                    {/*iterate through outputs to render a table per output category*/}
+                                    {parsed_outputs.map(output => {
                                         return(
                                             <TableRow
-                                                key={row[0]}
+                                                key={output[0]}
                                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                             >
-                                                <TableCell component="th" scope="row">{row[0]}</TableCell>
-                                                <TableCell align="right">{row[1]}</TableCell>
+                                                <TableCell component="th" scope="row">{output[0]}</TableCell>
+                                                <TableCell align="right">{output[1]}</TableCell>
                                             </TableRow>
-                                        )
+                                        );
                                     })}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                            );
-                        }
-                    })}
-                </AccordionDetails>
-            </Accordion>
-            {/*creating accordion view for outputs*/}
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Typography sx={{color: 'secondary.dark'}}>Outputs</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow sx={{bgcolor: 'secondary.light'}}>
-                                    <TableCell sx={{fontWeight: "bold"}}>Measurements</TableCell>
-                                    <TableCell sx={{fontWeight: 'bold'}} align="right"></TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody> 
-                                {/*iterate through outputs to render a table per output category*/}
-                                {parsed_outputs.map(output => {
-                                    return(
-                                        <TableRow
-                                            key={output[0]}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row">{output[0]}</TableCell>
-                                            <TableCell align="right">{output[1]}</TableCell>
-                                        </TableRow>
-                                    );
-                                })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </AccordionDetails>
-            </Accordion>
-        </Box>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </AccordionDetails>
+                </Accordion>
+            </Grid>
+        </Grid>
     );
 }
